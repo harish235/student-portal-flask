bef3ee941664dfb78d988cd6b5eeff09f1657cd4
Harish SS <harish.ss@gdn-commerce.com>
2025-01-31 20:03:43 +0530

    fixed teachers_test test_get_assignments_teacher_1, test_get_assignments_teacher_2

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -74,9 +74,10 @@ class Assignment(db.Model):
 
 
     @classmethod
-    def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
+    def mark_grade(cls, _id, grade, teacher_id, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == teacher_id, "Assignment doesnt belongs to this teacher")
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
 
         # Restrict principals from grading assignments in DRAFT state
@@ -95,8 +96,11 @@ class Assignment(db.Model):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(
+            cls.teacher_id == teacher_id, 
+            cls.state != AssignmentStateEnum.DRAFT
+            ).all()
 
     @classmethod
     def get_assignments_by_principal(cls):
